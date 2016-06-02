6bcd4543b4776a459ed5c62a92bfda99d4e923ca
jonathandavidblack <jonnybl915@gmail.com>
2016-06-02 12:07:48 -0400

    add movement

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,20 +5,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureRegion stand;
+	TextureRegion jump;
+
 	float x, y, xv, yv; //xv = x velocity...
 	static final float MAX_JUMP_VELOCITY = 2000;
-	static final float MAX_VELOCITY = 300;
+	static final float MAX_VELOCITY = 200;
 	static final float DECELERATOR = 0.99f;
 	static final int WIDTH = 18;
 	static final int HEIGHT = 28;
 	static final int GRAVITY = -50;
-	boolean canJump;
+	boolean canJump, faceRight = true;
+	Animation walk;
+	float time;
+
 	
 	@Override
 	public void create () {
@@ -26,16 +32,36 @@ public class MyGdxGame extends ApplicationAdapter {
 		Texture sheet = new Texture("koalio.png");
 		TextureRegion[][] tiles = TextureRegion.split(sheet, WIDTH, HEIGHT);
 		stand = tiles[0][0];
+		jump = tiles[0][1];
+		walk = new Animation(0.2f, tiles[0][2], tiles[0][3], tiles[0][4]);
 
 	}
 
 	@Override
 	public void render () {
 		move();
+
+		time += Gdx.graphics.getDeltaTime();
+
+		TextureRegion img;
+		if(y > 0){
+			img = jump;
+		}
+		else if(xv!=0){
+			img = walk.getKeyFrame(time, true);
+		}
+		else{
+			img = stand;
+		}
 		Gdx.gl.glClearColor(0.8f, 0.7f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(stand, x, y, WIDTH *3, HEIGHT *3);
+		if (faceRight){
+			batch.draw(img, x, y, WIDTH * 3, HEIGHT * 3);
+		}
+		else{
+			batch.draw(img, x +WIDTH * 3, y, WIDTH * -3, HEIGHT * 3);
+		}
 		batch.end();
 	}
 
@@ -49,9 +75,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 			xv = MAX_VELOCITY;
+			faceRight = true;
 		}
 		else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 			xv = -MAX_VELOCITY;
+			faceRight = false;
 		}
 
 		yv += GRAVITY;
@@ -67,8 +95,8 @@ public class MyGdxGame extends ApplicationAdapter {
 			y=0;
 			canJump = true;
 		}
-		if(x>800){
-			x=800;
+		if(x>948){ //*** not working ***
+			x=948;
 		}
 		if(x<0){
 			x=0;
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 800;
-		config.width = 800;
+		config.width = 1000;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
